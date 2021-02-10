2a5e6e66465105930b7126e2f7869210446ee10b
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-02-11 00:31:03 +0100

    add one to quiz position state on showNextQuestion function

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,9 @@ class Quiz extends Component {
     }
 
     showNextQuestion() {
-
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        })
     }
 
     render() {
